583db5cbbfcecf1965d58693e1b8c9f522476c14
Md. Tohid <sagormdtohid@gmail.com>
2026-03-31 21:28:25 +0600

    Working LabApplications UI

--- common/DropdownSelect.jsx
+++ common/DropdownSelect.jsx
@@ -26,28 +26,28 @@ const DropdownSelect = ({ filters = [], onChange }) => {
   }, []);
 
   return (
-    <div className="relative w-[176px] text-[#3D3D3D] redhat " ref={dropdownRef}>
+    <div className="relative w-[145px] text-[#DFE1E3] textLabel16 " ref={dropdownRef}>
       <button
-        className="w-full px-4 py-2 border border-[#B683D5] rounded-[10px] flex justify-between items-center text-left focus:outline-none hover:bg-[#ECE6F0]/20 transition duration-300"
+        className="w-full px-[14px] py-[13px] border-2 border-[#ABADAF] rounded-[10px] flex justify-between items-center text-left focus:outline-none bg-transparent hover:bg-[#313335] active:bg-transparent transition duration-300 "
         onClick={() => setIsOpen((prev) => !prev)}
       >
         <span>{selected}</span>
         <DownArrowIcon
-            className={`text-[#C4C4C4] transition-transform duration-300 ${
+            className={`text-[#DFE1E3] transition-transform duration-300 ${
             isOpen ? "-rotate-180" : ""
           }`}
         />
       </button>
 
       {isOpen && (
-        <ul className="absolute z-10 w-full mt-1 bg-white border border-[#E5E5E5] rounded-[5px] shadow-lg max-h-60 overflow-y-auto">
+        <ul className="absolute z-10 w-full mt-1 bg-transparent border border-[#E5E5E5] rounded-[5px] shadow-lg max-h-60 overflow-y-auto">
           {filters.map((filter) => (
             <li
               key={filter.key}
               onClick={() => handleSelect(filter)}
-              className={`text-[#242424] textBody16 px-4 py-2 border-l-[4px] border-transparent cursor-pointer hover:bg-slate-100 ${
+              className={`text-[#DFE1E3] textBody16 px-4 py-2 border-l-[4px] border-transparent cursor-pointer hover:bg-[#313335] ${
                 selected === filter.label
-                  ? "bg-white font-semibold !border-[#B683D5]"
+                  ? "bg-transparent font-semibold !border-transparent"
                   : ""
               }`}
             >
